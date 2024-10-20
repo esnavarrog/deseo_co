a563ed2d24fbfd89e86869c8832c98f4ac78d2f3
Esteban Navarro <esnavarrog@gmail.com>
2024-10-20 14:53:42 -0300

    afsdfaf

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,10 +15,11 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'                                           # required
   config.fog_credentials = {
+    endpoint: 'deseo-compartir-6tr89wxujweezzxryncah68ada6ghsae1a-s3alias'
     provider:              'AWS',                                           # required
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
-    region:                'sa-east-1',                                     # optional, defaults to 'us-east-1'
+    region:                'sa-east-1',                                # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'deseocompartir'                                  # required
   config.fog_attributes = {
