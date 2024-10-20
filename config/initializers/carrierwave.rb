4908977abef5556c5e41b153169ccc51029365a3
Esteban Navarro <esnavarrog@gmail.com>
2024-10-20 14:48:08 -0300

    adasda

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,8 +16,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                                           # required
   config.fog_credentials = {
     provider:              'AWS',                                           # required
-    aws_access_key_id:     Rails.application.credentials.aws_access_key_id,     # required
-    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key, # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
     region:                'sa-east-1',                                     # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'deseocompartir'                                  # required
